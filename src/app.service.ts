3be9446b9a317fe3a2cac4a4410a4f8e9d63bdc1
Ingo Cloß <ingo.clohs@icloud.com>
2020-12-13 13:25:07 +0100

    add socket-messages

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,55 @@
 import { Injectable } from '@nestjs/common';
+import { WebSocketGateway, WebSocketServer, SubscribeMessage, OnGatewayConnection, OnGatewayDisconnect } from '@nestjs/websockets';
 
+@WebSocketGateway()
 @Injectable()
-export class AppService {
-  getHello(): string {
-    return 'Hello World!';
-  }
+export class AppService implements OnGatewayConnection, OnGatewayDisconnect {
+  
+    @WebSocketServer()
+    private server;
+
+    private connectedClients: number = 0;
+
+    async handleConnection() {
+      // a client has connected
+      this.connectedClients++;
+
+      // notify connected clients of current users
+      this.server.emit('connectedclients', this.connectedClients);
+    }
+
+    async handleDisconnect() {
+      // a client has disconnected
+      this.connectedClients--;
+
+      // notify connected clients of current users
+      this.server.emit('connectedclients', this.connectedClients);
+    }
+
+    /**
+     * Message, wenn Master den Status einer Schätzung ändert (z.B. von OPEN zu RUNNING oder von RUNNING zu DONE).
+     * @param client 
+     * @param currentMaster 
+     * @param currentVote 
+     */
+    @SubscribeMessage('masterVoteChanged')
+    async onMasterVoteChanged(client, currentMaster, currentVote) {
+      client.broadcast.emit('masterVoteChanged', currentMaster, currentVote);
+    }
+
+    /** Message, wenn Member eine Schätzung speichert */
+    @SubscribeMessage('memberVoteChanged')
+    async onMemberVoteChanged(client, currentMember, currentVote) {
+      client.broadcast.emit('memberVoteChanged', currentMember, currentVote);
+    }
+
+    @SubscribeMessage('memberVotingStart')
+    async onMemberVotingStart(client, currentMember, currentVote) {
+      client.broadcast.emit('memberVotingStart', currentMember, currentVote);
+    }
+
+    @SubscribeMessage('memberVotingEnd')
+    async onMemberVotingEnd(client, currentMember, currentVote) {
+      client.broadcast.emit('memberVotingEnd', currentMember, currentVote);
+    }
 }

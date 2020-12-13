9b47f96d94f488a303a8fc6db99e3fa3669cd44b
Ingo Cloß <ingo.clohs@icloud.com>
2020-12-13 16:28:24 +0100

    ...

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const whitelist = ['http://localhost:8080', 'https://vote-alpha.vercel.app/'];
+  const whitelist = ['http://localhost:8080/', 'https://vote-alpha.vercel.app/'];
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: function (origin, callback) {

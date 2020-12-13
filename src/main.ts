e922e56b13d114004188d1413bdfa9ff279140a9
Ingo Cloß <ingo.clohs@icloud.com>
2020-12-13 16:18:25 +0100

    enableCors

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,20 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const whitelist = ['http://localhost:8080', 'https://vote-alpha.vercel.app/'];
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: function (origin, callback) {
+      if (whitelist.indexOf(origin) !== -1) {
+        callback(null, true)
+      } else {
+        callback(new Error('Not allowed by CORS'))
+      }
+    },
+    // TODO: allowedHeaders: 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Observe',
+    // TODO: methods: "GET,PUT,PATCH,POST,DELETE,UPDATE,OPTIONS",
+    credentials: true,
+  });
   await app.listen(process.env.PORT || 3200);
 }
 bootstrap();

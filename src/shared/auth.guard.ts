fe4eec7f2ca8234effb880285046fc5c1e6ba3cd
Ingo Cloß <ingo.clohs@icloud.com>
2020-12-16 08:18:07 +0100

    authGuard erstmal deativiert

--- src/shared/auth.guard.ts
+++ src/shared/auth.guard.ts
@@ -10,6 +10,10 @@ export class AuthGuard implements CanActivate {
   }
 
   canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
+      return true;
+  }
+  /*
+  canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
       const req = context.switchToHttp().getRequest();
       let authKey: string = req.headers.authorization;
       if (!authKey) {
@@ -20,4 +24,5 @@ export class AuthGuard implements CanActivate {
   catch (err) {
       return false;
   }
+  */
 }
